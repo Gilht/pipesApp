dc73794b23324009d105a6def868203116aabeba
Gilht <ghdzt14@hotmail.com>
2024-09-24 21:01:59 -0600

    added basics pipes to basics page

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from "primeng/menu";
 import { MenubarModule } from "primeng/menubar";
+import { CardModule } from "primeng/card";
+import { FieldsetModule } from "primeng/fieldset";
+import { ButtonModule } from "primeng/button";
+import { PanelModule } from "primeng/panel";
 
 @NgModule({
   exports: [
     MenubarModule,
-    MenuModule
+    MenuModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './basics-page.component.scss'
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = "gilberto";
+  public nameUpper: string = "GILBERTO";
+  public fullName: string = "GilbErTo HdZ";
 }
