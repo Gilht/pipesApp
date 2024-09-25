919b0542f2e9ed5a738450fbec9e0084805b02a3
Gilht <ghdzt14@hotmail.com>
2024-09-25 01:59:03 -0600

    cambiar de idioma mediante locale a cualquier idioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import localeEsMX from "@angular/common/locales/es-MX";
+import localeFrCA from "@angular/common/locales/fr-CA";
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsMX);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +19,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: "fr-CA"
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
